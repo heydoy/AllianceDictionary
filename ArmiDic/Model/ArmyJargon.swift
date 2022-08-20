e4276e6d32b88408d8e14db0ae72f0e6df1b3546
heydoy <heydoyhey@gmail.com>
2022-08-20 19:38:43 +0900

    JSON 사전 데이터 업로드, 사전 모델 생성

---
+++ ArmiDic/Model/ArmyJargon.swift
@@ -0,0 +1,18 @@
+//
+//  Dictionary.swift
+//  ArmiDic
+//
+//  Created by Doy Kim on 2022/08/20.
+//
+
+import Foundation
+
+
+struct ArmyJargon: Codable {
+    
+    // 한글, 약어, 영어
+    var ko: String
+    var abbr: String
+    var en: String
+
+}
--- ArmiDic/ViewController.swift
+++ ArmiDic/ViewController.swift
@@ -7,13 +7,16 @@
 
 import UIKit
 
-class ViewController: UIViewController {
 
+class ViewController: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
     }
 
 
 }
+
 
